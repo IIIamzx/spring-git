0ce0f8db35f384fd4bb2b2442ec77e686fe67134
zx <1735911125@qq.com>
2023-02-06 20:31:59 +0800

    commit 1

---
+++ src/main/java/com/zx/TestGit.java
@@ -0,0 +1,12 @@
+package com.zx;
+
+/**
+ * Data: 2023/2/6
+ * Author: zx
+ * Description:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git! 1");
+    }
+}
