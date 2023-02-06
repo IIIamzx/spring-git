26e43b119b1c48178feba5d50abedcd38b2d08c3
zx <1735911125@qq.com>
2023-02-06 20:33:22 +0800

    commit 2

--- src/main/java/com/zx/TestGit.java
+++ src/main/java/com/zx/TestGit.java
@@ -8,5 +8,6 @@ package com.zx;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git! 1");
+        System.out.println("Hello,Git! 2");
     }
 }
